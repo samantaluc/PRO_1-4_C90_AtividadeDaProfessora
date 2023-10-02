372708fcf0e6bb29e2f3fe52c188b8ba4895892a
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-10-02 11:30:31 -0300

    Update IssInfo.js

--- screens/IssInfo.js
+++ screens/IssInfo.js
@@ -14,48 +14,30 @@ export default class IssLocationScreen extends Component {
             location: {}
         };
     }
-
     componentDidMount() {
         this.getIssLocation()
-        try {
-            setInterval(async () => {
-                this.getIssLocation()
-            }, 5000);
-        } catch (e) {
-            console.log(e);
-        }
+        try { setInterval(async () => {  this.getIssLocation()    }, 5000);
+        } catch (e) {            console.log(e);  }
     }
 
     getIssLocation = () => {
         axios
-            .get("https://api.wheretheiss.at/v1/satellites/25544")
-            .then(response => {
-                this.setState({ location: response.data })
-            })
-            .catch(error => {
-                Alert.alert(error.message)
-            })
+          .get("https://api.wheretheiss.at/v1/satellites/25544")
+          .then(response => {     this.setState({ location: response.data }) })
+          .catch(error => {Alert.alert(error.message)   })
     }
-
     render() {
         if (Object.keys(this.state.location).length === 0) {
-            return (
-                <View
-                    style={{
-                        flex: 1,
-                        justifyContent: "center",
-                        alignItems: "center"
-                    }}>
+            return ( <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
                     <Text>Carregando</Text>
                 </View>
             )
-        } else {
-            return (
-                <View style={styles.infoContainer}>
-                    <Text style={styles.infoText}>Latitude: {this.state.location.latitude}</Text>
-                    <Text style={styles.infoText}>Longitude: {this.state.location.longitude}</Text>
-                    <Text style={styles.infoText}>Altitude (KM): {this.state.location.altitude}</Text>
-                    <Text style={styles.infoText}>Velocidade (KM/H): {this.state.location.velocity}</Text>
+        } else { return (
+             <View style={styles.infoContainer}>
+              <Text style={styles.infoText}>Latitude: {this.state.location.latitude}</Text>
+              <Text style={styles.infoText}>Longitude: {this.state.location.longitude}</Text>
+              <Text style={styles.infoText}>Altitude (KM): {this.state.location.altitude}</Text>
+              <Text style={styles.infoText}>Velocidade (KM/H): {this.state.location.velocity}</Text>
                 </View>
             );
         }
